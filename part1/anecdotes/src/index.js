3d95fa3fb99588fe49495baa6810e268b0e52d0d
Antonio Gil <antonioluisgil@gmail.com>
2021-08-02 19:12:17 +0200

    add: Create anecdotes app

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0);
+
+  const handleClick = () => {
+    setSelected(getRandomInt(0, anecdotes.length - 1));
+  };
+
+  return (
+    <div>
+      <div>{props.anecdotes[selected]}</div>
+      <button onClick={handleClick}>Next anecdote</button>
+    </div>
+  );
+};
+
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
