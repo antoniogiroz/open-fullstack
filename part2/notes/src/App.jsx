9931cbef94471baf7dffeafa6e4426a02fdfda9c
Antonio Gil <antonioluisgil@gmail.com>
2021-10-24 16:13:39 +0200

    feat(notes): Add db with json-server and axios

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,44 +1,24 @@
-import { useState } from 'react';
-import logo from './assets/images/logo.svg';
-import './assets/css/App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [notes, setNotes] = useState([]);
+  // const [newNote, setNewNote] = useState('');
+  // const [showAll, setShowAll] = useState(true);
+
+  useEffect(async () => {
+    const { data } = await axios.get('http://localhost:3001/notes');
+    setNotes(data);
+  }, []);
+
+  console.log('render', notes.length, 'notes');
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id}>{note.content}</li>
+      ))}
+    </ul>
   );
 }
 
